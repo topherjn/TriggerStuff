7c8dea56b7ab437e6618af5a52b039b9a062a325
topherjn <nicholsonc@gmail.com>
2024-01-10 18:21:19 -0500

    added some comments

--- trigger.sql
+++ trigger.sql
@@ -5,9 +5,7 @@ AFTER INSERT ON orderdetails
    FOR EACH ROW 
       BEGIN  
       DECLARE curqty INT;   
-      DECLARE newStatus VARCHAR(15);
-      SET newStatus = 'In Process'; -- assume success
-      
+
       -- retrieve current QOH into a variable
       SET curqty = (SELECT quantityInStock       
 				    FROM products
@@ -16,7 +14,6 @@ AFTER INSERT ON orderdetails
    -- if the qty ordered is too much
    -- yell at user.  
    IF curqty - NEW.quantityordered < 0 THEN
-       SET newStatus = 'Cancelled'; -- note somewhere in DB that order is bad
 	   SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock cannot be negative'; 
    ELSE -- not too much ordered so decrement product QOH by qty ordered
 	   UPDATE products 
