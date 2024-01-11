72132f304e8dca26a50e885f81995f90fb47ca36
topherjn <nicholsonc@gmail.com>
2024-01-10 23:06:07 -0500

    insert command issues

--- TriggerTask3.sql
+++ TriggerTask3.sql
@@ -2,15 +2,41 @@
 
 -- Cheat at creating the table
 -- drop table orderslog;
-create table orderslog as
-select * from orders;
+-- create table orderslog as
+-- select * from orders;
 
 -- alter table to  have distinct rows
-alter table orderslog
-add column changeID INT primary key auto_increment;
-alter table orderslog
-add foreign key (ordernumber) references orders(ordernumber);
-delete from orderslog;
+-- alter table orderslog
+-- add column changeID INT primary key auto_increment;
+-- alter table orderslog
+-- add foreign key (ordernumber) references orders(ordernumber);
+-- delete from orderslog;
+-- added timestamp column changedTime with GUI
 
 -- Trigger
-
+DROP TRIGGER IF EXISTS log_changes;
+delimiter $$
+CREATE TRIGGER log_changes 
+AFTER INSERT ON orders 
+   FOR EACH ROW 
+      BEGIN  
+      
+      INSERT INTO orderslog (orderNumber, 
+                            orderDate,
+                            requiredDate,
+                            shippedDate,
+                            `status`,
+                            comments,
+                            customerNumber)
+      SELECT ordernumber, 
+             orderdate, 
+             requireddate, 
+             shippeddate, 
+             `status`, 
+             comments, 
+             customerNumber
+      FROM orders;
+     
+      END$$
+ 
+ DELIMITER ;
