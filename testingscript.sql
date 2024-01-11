656a67ed6c5a8909677b8a6afb3fa07938d82149
topherjn <nicholsonc@gmail.com>
2024-01-10 19:04:48 -0500

    Inversed the logic, but this will rollback no matter what if there is an error

--- testingscript.sql
+++ testingscript.sql
@@ -1,7 +1,7 @@
 SET @nextordernum = (select max(ordernumber) + 1 from orders);
  
-insert into orders values (@nextordernum,curdate(),curdate(),curdate(), 'Shipped','blah',103);
-insert into orderdetails values(@nextordernum, 'S10_1678', 1, 100.0, 1);
+insert into orders values (@nextordernum,curdate(),curdate(),curdate(), 'Shipped','reject this',103);
+insert into orderdetails values(@nextordernum, 'S10_1678', 1, 1000000.0, 1);
 
 select * from orders where ordernumber	= @nextordernum;
 -- select * from orderdetails where ordernumber = @nextordernum;
--- trigger.sql
+++ trigger.sql
@@ -6,7 +6,7 @@ AFTER INSERT ON orderdetails
       BEGIN  
       DECLARE curqty INT;   
       
-      UPDATE orders SET `status` = 'In Progress' WHERE ordernumber = NEW.ordernumber;
+      UPDATE orders SET `status` = 'Rejected' WHERE ordernumber = NEW.ordernumber;
       
       -- retrieve current QOH into a variable
       SET curqty = (SELECT quantityInStock       
@@ -17,13 +17,14 @@ AFTER INSERT ON orderdetails
    -- yell at user.  
    IF curqty - NEW.quantityordered < 0 THEN
        UPDATE orders SET `status` = 'Cancelled' WHERE ordernumber = NEW.ordernumber;
-       
 	   SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock cannot be negative'; 
    ELSE -- not too much ordered so decrement product QOH by qty ordered
 	   UPDATE products 
 	   SET
 			quantityinstock = quantityinstock - new.quantityordered
 	   WHERE productcode = NEW.productcode;
+       
+       UPDATE orders SET `status` = 'In Progress' WHERE ordernumber = NEW.ordernumber;
    
    END IF; 
    
