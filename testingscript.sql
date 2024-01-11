1919fd42ca3649207e677ad0209c059afbe54db5
topherjn <nicholsonc@gmail.com>
2024-01-10 19:26:46 -0500

    Changed tack: the signal causes a ROLLBACK so I stop using it and signal through order status instead

--- testingscript.sql
+++ testingscript.sql
@@ -1,7 +1,8 @@
 SET @nextordernum = (select max(ordernumber) + 1 from orders);
  
 insert into orders values (@nextordernum,curdate(),curdate(),curdate(), 'Shipped','reject this',103);
-insert into orderdetails values(@nextordernum, 'S10_1678', 1, 1000000.0, 1);
+insert into orderdetails values(@nextordernum, 'S10_1678', 1000000, 100.0, 1);
 
 select * from orders where ordernumber	= @nextordernum;
+-- select * from products where productcode = 'S10_1678';
 -- select * from orderdetails where ordernumber = @nextordernum;
--- trigger.sql
+++ trigger.sql
@@ -5,8 +5,8 @@ AFTER INSERT ON orderdetails
    FOR EACH ROW 
       BEGIN  
       DECLARE curqty INT;   
-      
-      UPDATE orders SET `status` = 'Rejected' WHERE ordernumber = NEW.ordernumber;
+      DECLARE newStatus VARCHAR(15);
+      SET newStatus = 'In Progress';
       
       -- retrieve current QOH into a variable
       SET curqty = (SELECT quantityInStock       
@@ -16,18 +16,18 @@ AFTER INSERT ON orderdetails
    -- if the qty ordered is too much
    -- yell at user.  
    IF curqty - NEW.quantityordered < 0 THEN
-	   SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock cannot be negative'; 
+	   SET newStatus = 'Rejected';
    ELSE -- not too much ordered so decrement product QOH by qty ordered
-	   UPDATE products 
+	   SET newStatus = 'In Progress';
+       
+       UPDATE products 
 	   SET
 			quantityinstock = quantityinstock - new.quantityordered
 	   WHERE productcode = NEW.productcode;
-       
-       UPDATE orders SET `status` = 'In Progress' WHERE ordernumber = NEW.ordernumber;
-   
+
    END IF; 
    
-   
+   UPDATE orders SET `status` = newStatus WHERE ordernumber = NEW.ordernumber;
    
  END$$
  
