fc9cf6f040185a930e630e72569dcf63071d1689
topherjn <nicholsonc@gmail.com>
2024-01-10 19:05:54 -0500

    Forget to save on last commit

--- trigger.sql
+++ trigger.sql
@@ -16,7 +16,6 @@ AFTER INSERT ON orderdetails
    -- if the qty ordered is too much
    -- yell at user.  
    IF curqty - NEW.quantityordered < 0 THEN
-       UPDATE orders SET `status` = 'Cancelled' WHERE ordernumber = NEW.ordernumber;
 	   SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock cannot be negative'; 
    ELSE -- not too much ordered so decrement product QOH by qty ordered
 	   UPDATE products 
